de6d8b0f01c4081dd9caf618027dba345322d443
francois.segaud@gmail.com <francois.segaud@gmail.com@9907bcb7-4bd5-630c-60af-82a6609e315a>
2014-05-03 11:04:55 +0000

    Changes the simulation descriptor class to handle multiple tags.

--- Loupcorne/Assets/Scripts/#Wip/KingTest.cs
+++ Loupcorne/Assets/Scripts/#Wip/KingTest.cs
@@ -5,7 +5,7 @@ public class KingTest : SimObjectWrapper
 {
 	void Start ()
     {
-        this.simObject = LoupCorne.Framework.Database.Instance.GetDatatable<LoupCorne.Framework.SimObject>().GetElement("King");
+        this.simObject = LoupCorne.Framework.Database.Instance.GetDatatable<LoupCorne.Framework.SimObject>().GetElement("King").Clone() as LoupCorne.Framework.SimObject;
         this.Refresh();
 	}
 
@@ -14,19 +14,25 @@ public class KingTest : SimObjectWrapper
         GUI.Label(new Rect(10, 10, 200, 20), string.Format("Stamina={0}", this.GetPropertyValue(SimProperties.Stamina)));
         GUI.Label(new Rect(10, 30, 200, 20), string.Format("Strength={0}", this.GetPropertyValue(SimProperties.Strength)));
 
-        if (GUI.Button(new Rect(10, 100, 100, 20), "HolySword"))
+        if (GUI.Button(new Rect(10, 100, 100, 20), "+HolySword"))
         {
             LoupCorne.Framework.SimDescriptor d = LoupCorne.Framework.Database.Instance.GetDatatable<LoupCorne.Framework.SimDescriptor>().GetElement("HolySword");
-            this.RemoveDescriptorByClass("WeaponItem");
+            this.RemoveDescriptorByClass("Weapon");
             this.AddDescriptor(d);
             this.Refresh();
         }
 
-        if (GUI.Button(new Rect(10, 120, 100, 20), "HolyArmor"))
+        if (GUI.Button(new Rect(10, 120, 100, 20), "+HolyArmor"))
         {
             LoupCorne.Framework.SimDescriptor d = LoupCorne.Framework.Database.Instance.GetDatatable<LoupCorne.Framework.SimDescriptor>().GetElement("HolyArmor");
-            this.RemoveDescriptorByClass("ChestItem");
+            this.RemoveDescriptorByClass("Chest");
             this.AddDescriptor(d);
+            this.Refresh();
+        }
+
+        if (GUI.Button(new Rect(10, 140, 100, 20), "DropAll"))
+        {
+            this.RemoveDescriptorByClass("Item");
             this.Refresh();
         }
     }
