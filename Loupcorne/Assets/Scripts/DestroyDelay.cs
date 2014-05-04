1b60489a66e09d85375802eec20a8a7a776fb95c
francois.segaud@gmail.com <francois.segaud@gmail.com@9907bcb7-4bd5-630c-60af-82a6609e315a>
2014-05-04 00:54:12 +0000

    Added a detroy after delay for Gfx.

---
+++ Loupcorne/Assets/Scripts/DestroyDelay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyDelay : MonoBehaviour
+{
+    [SerializeField]
+    private float delay;
+
+	void Start ()
+    {
+        StartCoroutine(this.RunAsync());
+	}
+
+    private IEnumerator RunAsync()
+    {
+        yield return new WaitForSeconds(this.delay);
+
+        GameObject.Destroy(this.gameObject);
+        yield break;
+    }
+}
