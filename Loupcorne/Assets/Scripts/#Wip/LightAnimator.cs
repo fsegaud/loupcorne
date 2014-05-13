b217e499dc3366ff24d3a0364ffe309aa7bf2280
francois.segaud@gmail.com <francois.segaud@gmail.com@9907bcb7-4bd5-630c-60af-82a6609e315a>
2014-05-13 20:43:08 +0000

    Added the main menu screen.

---
+++ Loupcorne/Assets/Scripts/#Wip/LightAnimator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light))]
+public class LightAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private float min;
+
+    [SerializeField]
+    private float max;
+
+    [SerializeField]
+    private float speed;
+
+	void Update ()
+    {
+        this.light.intensity = this.min + Mathf.PingPong(Time.time * this.speed, this.max - this.min);
+	}
+}
---
+++ Loupcorne/Assets/Scripts/UI/UIMainMenuPanel.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class UIMainMenuPanel : UIPanel
+{
+    public enum State
+    {
+        Main,
+        Manual,
+        Credits,
+        Loading
+    }
+
+    private State state;
+
+    [SerializeField]
+    private Texture loadingTexture;
+
+    protected override void Draw()
+    {
+        base.Draw();
+
+        switch (this.state)
+        {
+            case State.Main:
+                this.Draw_Main();
+                break;
+
+            case State.Credits:
+                this.Draw_Credits();
+                break;
+
+            case State.Manual:
+                this.Draw_Manual();
+                break;
+
+            case State.Loading:
+                this.Draw_Loading();
+                break;
+        }
+    }
+
+    private void Draw_Main()
+    {
+        base.Draw();
+
+        GUI.BeginGroup(new Rect(32f, Screen.height * .5f - 185f, 300f, 370f), string.Empty);
+        {
+            GUI.Box(new Rect(0f, 0f, 300f, 370f), string.Empty);
+
+            if (GUI.Button(new Rect(10f, 10f, 280f, 80f), "NEW GAME"))
+            {
+                this.state = State.Loading;
+                Application.LoadLevel("3C");
+            }
+
+            if (GUI.Button(new Rect(10f, 100f, 280f, 80f), "MANUAL"))
+            {
+                this.state = State.Manual;
+            }
+
+            if (GUI.Button(new Rect(10f, 190f, 280f, 80f), "CREDITS"))
+            {
+                this.state = State.Credits;
+            }
+
+            if (GUI.Button(new Rect(10f, 280f, 280f, 80f), "QUIT TO DESKTOP"))
+            {
+                Application.Quit();
+            }
+        }
+        GUI.EndGroup();
+    }
+
+    private void Draw_Credits()
+    {
+        base.Draw();
+
+        GUI.BeginGroup(new Rect(32f, Screen.height * .5f - 185f, 300f, 370f), string.Empty);
+        {
+            GUI.Box(new Rect(0f, 0f, 300f, 370f), string.Empty);
+
+            GUI.Label(new Rect(10f, 5f, 280f, 270f), Content.Credits, "credits");
+
+            if (GUI.Button(new Rect(10f, 280f, 280f, 80f), "BACK"))
+            {
+                this.state = State.Main;
+            }
+        }
+        GUI.EndGroup();
+    }
+
+    private void Draw_Manual()
+    {
+        base.Draw();
+
+        GUI.BeginGroup(new Rect(32f, Screen.height * .5f - 185f, 300f, 370f), string.Empty);
+        {
+            GUI.Box(new Rect(0f, 0f, 300f, 370f), string.Empty);
+
+            if (GUI.Button(new Rect(10f, 280f, 280f, 80f), "BACK"))
+            {
+                this.state = State.Main;
+            }
+        }
+        GUI.EndGroup();
+    }
+
+    private void Draw_Loading()
+    {
+        GUI.DrawTexture(new Rect(0f, 0f, Screen.width, Screen.height), this.loadingTexture);
+
+        GUI.Label(new Rect(Screen.width - 180f, Screen.height - 40f, 180f, 32f), "Loading...", "loading");
+    }
+
+    private class Content
+    {
+        public static readonly string Credits = string.Empty
+            + "<color=#FFC000>Game Design</color>\nKevin Nguyen Kim Tuoï\n\n"
+            + "<color=#FFC000>Programming</color>\nPierre Maury\nFrançois Ségaud\n\n"
+            + "<color=#FFC000>Art</color>\nBaptiste Doux\nOlivier Leroy\n\n"
+            + "<color=#FFC000>Music & Sound Design</color>\nMarc-Antoine Archier\n\n"
+            + "<color=#FFC000>Resources</color>\ncgtextures.com\ngame-icons.net\nhttp://dafont.com";
+    }
+}
