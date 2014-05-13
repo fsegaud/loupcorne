88c7fe7cc417bc166b435afc3058407f80676251
francois.segaud@gmail.com <francois.segaud@gmail.com@9907bcb7-4bd5-630c-60af-82a6609e315a>
2014-05-13 20:43:08 +0000

    Added the main menu screen.

--- trunk/Loupcorne/Assets/Scripts/UI/UIObjectivePanel.cs
+++ trunk/Loupcorne/Assets/Scripts/UI/UIObjectivePanel.cs
@@ -6,22 +6,27 @@ public class UIObjectivePanel : UIPanel
     private int maxPeons;
     private int maxGuards;
 
+    public float x1, y1, x2, y2;
+
     protected override void Draw()
     {
         base.Draw();
 
-        GUI.BeginGroup(new Rect(Screen.width - 115f, 0f, 115f, 94f), string.Empty);
+        GUI.BeginGroup(new Rect(Screen.width - 209, 0f, 209f, 97f), string.Empty);
         {
             // Background.
-            GUI.Box(new Rect(0f, 0f, 115f, 94f), string.Empty, "background");
+            GUI.Box(new Rect(0f, 0f, 209f, 97f), string.Empty, "background");
 
             // Remaining peons.
-            GUI.Label(new Rect(8f + 10, 8f + 4, 50f, 28f), string.Format("{0}<color=#808080>/{1}</color>", UnitsManager.Instance.peons.Count, this.maxPeons), "peons");
-            GUI.Label(new Rect(64f + 12, 8f + 4, 28f, 28f), string.Empty, "peon_icon");
+            GUI.Label(new Rect(8f + 40f, 8f + 4f, 50f, 28f), string.Format("{0}<color=#808080>/{1}</color>", UnitsManager.Instance.peons.Count, this.maxPeons), "peons");
+            GUI.Label(new Rect(64f + -40f, 8f + 4f, 28f, 28f), string.Empty, "peon_icon");
             
             // Remaining guards.
-            GUI.Label(new Rect(8f + 10, 44f + 4, 50f, 28f), string.Format("{0}<color=#808080>/{1}</color>", UnitsManager.Instance.guards.Count, this.maxGuards), "guards");
-            GUI.Label(new Rect(64f + 12, 44f + 4, 28f, 28f), string.Empty, "guard_icon");
+            GUI.Label(new Rect(8f + 128f, 8f + 4, 50f, 28f), string.Format("{0}<color=#808080>/{1}</color>", UnitsManager.Instance.guards.Count, this.maxGuards), "guards");
+            GUI.Label(new Rect(64f + 48f, 8f + 4, 28f, 28f), string.Empty, "guard_icon");
+
+            // Timer
+            GUI.Label(new Rect(0f, 64f, 209f, 0f), "00:00", "timer");
         }
         GUI.EndGroup();
     }
