3382fdb7f8be7df47d3a17c8514432566df3925f
francois.segaud@gmail.com <francois.segaud@gmail.com@9907bcb7-4bd5-630c-60af-82a6609e315a>
2014-05-17 13:41:28 +0000

    Added health bars on entities. Added the main menu's manual.

---
+++ trunk/Loupcorne/Assets/Scripts/UI/UIELement.cs
@@ -0,0 +1,24 @@
+﻿
+public class UIELement : LoupCorne.Framework.DatatableElement
+{
+    [System.Xml.Serialization.XmlElement]
+    public string Title
+    {
+        get;
+        protected set;
+    }
+
+    [System.Xml.Serialization.XmlElement]
+    public string Description
+    {
+        get;
+        protected set;
+    }
+
+    [System.Xml.Serialization.XmlElement]
+    public string TexturePath
+    {
+        get;
+        protected set;
+    }
+}
