c31a5f9139092be0881050f937f84d67b334d873
francois.segaud@gmail.com <francois.segaud@gmail.com@9907bcb7-4bd5-630c-60af-82a6609e315a>
2014-05-18 21:07:31 +0000

    Dev fait sur le stand.

---
+++ Loupcorne/Assets/Scripts/#Wip/ShakeThatCam.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShakeThatCam : MonoBehaviour
+{
+    [SerializeField]
+    private int numPoints;
+
+    [SerializeField]
+    private float maxOffset;
+
+    private Queue<Vector3> points = new Queue<Vector3>();
+    private Vector3 localPosOrig;
+
+    void Start()
+    {
+        this.localPosOrig = this.transform.localPosition;
+    }
+
+    void Update()
+    {
+        if (this.points.Count > 0)
+        {
+            this.transform.localPosition = this.localPosOrig + this.points.Dequeue();
+        }
+        else
+        {
+            this.transform.localPosition = this.localPosOrig;
+        }
+    }
+
+    public void Shake()
+    {
+        for (int i = 0; i < this.numPoints; ++i)
+        {
+            this.points.Enqueue(new Vector3(Random.Range(0f, this.maxOffset), Random.Range(0f, this.maxOffset), Random.Range(0f, this.maxOffset)));
+        }
+    }
+}
